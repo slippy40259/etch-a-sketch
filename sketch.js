2a8f98632395a27162bf9d1693a3508a6943a0a9
Slippy <andrewchung667@gmail.com>
2025-07-12 22:57:12 -0500

    Added a hovering effect for sketching - When the mouse enters and leaves the grid, it paints the grid - Width changed to 768px

--- sketch.js
+++ sketch.js
@@ -4,5 +4,8 @@ const SIZE = 16;
 for (let i = 0; i < SIZE**2; i++) {
     let grid = document.createElement("div");
     grid.classList.add("grid");
+    grid.addEventListener("mouseleave", function(e) {
+        e.target.style.background = "blue";
+    })
     sketchBox.appendChild(grid);
 }
