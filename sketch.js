a7dd4d16ae4f7897adc51fb1ae7563695cb68b1d
Slippy <andrewchung667@gmail.com>
2025-07-13 23:12:13 -0500

    Added a resize button for etch-a-sketch - Refactored blocks of code into generateSketch and main - main generates the first sketch and allows user to resize with input - The button is postioned at the top of the screen

--- sketch.js
+++ sketch.js
@@ -1,11 +1,42 @@
-let sketchBox = document.querySelector(".container");
-const SIZE = 16;
+const sketchBox = document.querySelector(".container");
+const BOX_SIZE = 960;
+const button = document.querySelector("button");
 
-for (let i = 0; i < SIZE**2; i++) {
-    let grid = document.createElement("div");
-    grid.classList.add("grid");
-    grid.addEventListener("mouseleave", function(e) {
-        e.target.style.background = "blue";
-    })
-    sketchBox.appendChild(grid);
+function generateSketch(gridNum) {
+    let gridSize = BOX_SIZE/gridNum;
+    sketchBox.style.width = BOX_SIZE.toString() + "px";
+    for (let i = 0; i < gridNum**2; i++) {
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
+        grid.style.width = gridSize.toString() + "px";
+        grid.style.height = gridSize.toString() + "px";
+        grid.addEventListener("mouseleave", function(e) {
+            e.target.style.background = "blue";
+        })
+
+        sketchBox.appendChild(grid);
+    }
 }
+
+function removeSketch(grid) {
+    sketchBox.removeChild(grid);
+}
+
+function main() {
+    let gridNum = 16;
+    generateSketch(gridNum);
+
+    button.addEventListener("click", function() {
+        gridNum = prompt("Enter a size of the sketch box");
+        if (gridNum > 100) {
+            alert("The size cannot be larger than 100");
+        }
+        else {
+            let grids = document.querySelectorAll(".grid");
+            grids.forEach(removeSketch);
+            generateSketch(gridNum);
+        }
+    })
+}
+
+main();
